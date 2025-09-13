b8c492328713aaa59f3641bafcc7714279423ee3
hs_code <sublhs030304@gamil.com>
2025-09-13 15:57:36 +0900

    feat : 수 비교

---
+++ JavaEx/FirstWeekStudy/Study_B_A1330.java
@@ -0,0 +1,28 @@
+package JavaEx.FirstWeekStudy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Study_B_A1330 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("두 수를 입력 해주세요");
+
+        try{
+        double numA = Integer.parseInt(br.readLine());
+        double numB = Integer.parseInt(br.readLine());
+        if (numA > numB){
+            System.out.println(" > ");
+        }else if (numA < numB){
+            System.out.println(" < ");
+        } else {
+            System.out.println( " == ");
+        }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        
+    } //main
+} //class
